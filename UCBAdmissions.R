1ce68be2be37f0faeb446f8b4c4240e1d84119f0
veronika <Veronicka98@mail.ru>
2017-03-23 19:52:52 +0000

    ucbadmissions

--- UCBAdmissions.R
+++ UCBAdmissions.R
@@ -8,47 +8,13 @@ tmp
 admit <- tmp[,c("Admit", "Gender", "Dept")]
 admit
 
+dept <- "A"
+all <- sum(admit$Dept==dept)
+pf <- sum(admit$Gender=="Female" & admit$Dept == dept)/all
+pfanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Female" & admit$Dept==dept)/all
+pfanda/pf
+pm <- sum(admit$Gender=="Male" & admit$Dept==dept)/all
+pmanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Male" & admit$Dept==dept)/all
+pmanda/pm
 
-all <- sum(admit$Dept=="A")
-pf <- sum(admit$Gender=="Female" & admit$Dept == "A")/all
-pfanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Female" & admit$Dept=="A")/all
-pfanda/pf
-pm <- sum(admit$Gender=="Male" & admit$Dept=="A")/all
-pmanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Male" & admit$Dept=="A")/all
-pmanda/pm
-all <- sum(admit$Dept=="B")
-pf <- sum(admit$Gender=="Female" & admit$Dept == "B")/all
-pfanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Female" & admit$Dept=="B")/all
-pfanda/pf
-pm <- sum(admit$Gender=="Male" & admit$Dept=="B")/all
-pmanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Male" & admit$Dept=="B")/all
-pmanda/pm
-all <- sum(admit$Dept=="C")
-pf <- sum(admit$Gender=="Female" & admit$Dept == "C")/all
-pfanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Female" & admit$Dept=="C")/all
-pfanda/pf
-pm <- sum(admit$Gender=="Male" & admit$Dept=="C")/all
-pmanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Male" & admit$Dept=="C")/all
-pmanda/pm
-all <- sum(admit$Dept=="D")
-pf <- sum(admit$Gender=="Female" & admit$Dept == "D")/all
-pfanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Female" & admit$Dept=="D")/all
-pfanda/pf
-pm <- sum(admit$Gender=="Male" & admit$Dept=="D")/all
-pmanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Male" & admit$Dept=="D")/all
-pmanda/pm
-all <- sum(admit$Dept=="E")
-pf <- sum(admit$Gender=="Female" & admit$Dept == "E")/all
-pfanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Female" & admit$Dept=="E")/all
-pfanda/pf
-pm <- sum(admit$Gender=="Male" & admit$Dept=="E")/all
-pmanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Male" & admit$Dept=="E")/all
-pmanda/pm
-all <- sum(admit$Dept=="F")
-pf <- sum(admit$Gender=="Female" & admit$Dept == "F")/all
-pfanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Female" & admit$Dept=="F")/all
-pfanda/pf
-pm <- sum(admit$Gender=="Male" & admit$Dept=="F")/all
-pmanda <- sum(admit$Admit=="Admitted" & admit$Gender=="Male" & admit$Dept=="F")/all
-pmanda/pm
 
